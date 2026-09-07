e5ecabcfb9015e3271c89a9970ed708a1941b5bb
geroluna44 <geroluna44@proton.me>
2026-09-07 03:26:44 -0300

    Activar comentarios con Giscus

--- site.config.js
+++ site.config.js
@@ -15,10 +15,10 @@ export default {
     // Configuración de Giscus. Deja vacío para desactivar.
     // Requiere un repo de GitHub con Discussions habilitado.
     giscus: {
-      repo: "",
-      repoId: "",
+      repo: "geroluna44/blog",
+      repoId: "R_kgDOUQx7zw",
       category: "Announcements",
-      categoryId: "",
+      categoryId: "DIC_kwDOUQx7z84DFDKC",
       mapping: "pathname",
       strict: "0",
       reactionsEnabled: "1",
